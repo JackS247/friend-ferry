3194d818d429d29ddd877076340bf1f0f68de052
Jack Snape <jacksnape@Jacks-MBP.fritz.box>
2023-02-06 17:36:43 +0100

    latest three

--- server.js
+++ server.js
@@ -5,19 +5,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080 || process.env.PORT;
 const cors = require("cors");
-// const postsRoute = require("./routes/posts");
+
 const userRouter = require("./routes/userRoute");
 const companionRouter = require("./routes/companionRoute");
-// const User = require("./models/User");
 
 // middlewares
 app.use(express.json());
 app.use(cors());
-// app.use("/posts", postsRoute);
+
 app.use("/users", userRouter);
 app.use("/companions", companionRouter);
-
-// express.urlencoded({extended: false})
 
 app.get("/", (req, res) => {
   res.send("WELCOME TO MY API");
